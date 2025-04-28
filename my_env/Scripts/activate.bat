6628886c2dd3aad98842ce437a2a524de184f324
7BharathKumar <bharath2003kumarp@gmail.com>
2025-04-28 13:23:21 +0530

    Deploy Project

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\5. Project\Thyroid-Disease-Detection\Thyroid-Disease-Detection-main\my_env"
+set "VIRTUAL_ENV=D:\Open-Source Projects\Thyroid-Prediction-System\my_env"
 
 if not defined PROMPT set PROMPT=$P$G
 
